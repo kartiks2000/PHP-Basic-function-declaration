18286770c476a6c9ea6628316489ff99dea4850c
kartiks2000 <46376692+kartiks2000@users.noreply.github.com>
2020-08-02 15:48:23 +0530

    Add files via upload

---
+++ dem.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Function in PHP</h1>
+    <?php    
+
+        function sayhello(){
+            echo "hello bro!!!";
+        }
+
+        sayhello();
+    ?>
+</body>
+</html>
